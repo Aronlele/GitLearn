84d217f2628497a6576c03fafb6f7f8d1e99ba4b
薛乐乐 <xuelele@haier.com>
2022-11-10 18:10:45 +0800

    第一次提交，主分支

---
+++ com.test.huc.git/TestFirst.java
@@ -0,0 +1,14 @@
+package com.test.huc.git;
+
+/**
+ *
+ * @author xuelele
+ * @since 2022/11/10 18:08
+ */
+public class TestFirst {
+
+    public static void main(String[] args) {
+        System.out.println("打印下");
+
+    }
+}
