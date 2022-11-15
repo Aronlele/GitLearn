b7bfce5afc7fa38716ef88af34ecd890ad87ee6b
薛乐乐 <Aronqust@gmail.com>
2022-11-15 10:41:51 +0800

    Update TestFirst.java
    origin main change 1

--- com.test.huc.git/TestFirst.java
+++ com.test.huc.git/TestFirst.java
@@ -10,6 +10,7 @@ public class TestFirst {
     public static void main(String[] args) {
         System.out.println("打印下");
         // main feature 第一次
+        // origin main change 1 
 
     }
 }
